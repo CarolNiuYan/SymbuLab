8e9a8146d36a4bd2434f79a4819b72dd26178bde
CarolNiuYan <yanxinyi@grinnell.edu>
2018-08-15 14:56:02 -0500

    modified 14

--- SymbulationEmp/source/native/symbulation.cc
+++ SymbulationEmp/source/native/symbulation.cc
@@ -7,14 +7,14 @@
 using namespace std;
 
 EMP_BUILD_CONFIG( SymConfigBase,
-                 VALUE(SEED, double, 10, "What value should the random seed be?"),
+                 VALUE(SEED, double, 20, "What value should the random seed be?"),
                    VALUE(WORLD_TYPE, int, 1, "Type of the world (1 for Mixed and 2 for Grid"),
-                   VALUE(UPDATES, int, 1, "Number of updates to run before quitting"),
+                   VALUE(UPDATES, int, 10, "Number of updates to run before quitting"),
                   VALUE(ECOLI_NUM, size_t, 200, "Initial number of E.coli bacteria"),
                   VALUE(SHIGELLA_NUM, size_t, 200, "Initial number of shigella bacteria"),
                   VALUE(MOI_E, double, 1.0, "Initial MOI of E.coli Virus"),
                   VALUE(MOI_S, double, 1.0, "Initial MOI of shigella Virus"),
-                  VALUE(BURSTVAL, double, 10, "Burst Timer"),
+                  VALUE(BURST_VAL, double, 10, "Burst Timer"),
                   VALUE(REPRO_RATE, double, 1.0, "Rate of reproduction for symbiont"),
                  )
 	
@@ -37,8 +37,9 @@ int main(int argc, char * argv[])
     double MOI_E = config.MOI_E();
     double MOI_S = config.MOI_S();
     double POP_SIZE = ECOLI_NUM + SHIGELLA_NUM;
-    double BURSTVAL = config.BURSTVAL();
+    double BURSTVAL = config.BURST_VAL();
     double REPRO_RATE = config.REPRO_RATE();
+    double resource = (ECOLI_NUM + SHIGELLA_NUM)*28;
     
     emp::Random random(config.SEED());
         
@@ -47,7 +48,7 @@ int main(int argc, char * argv[])
     
     //Set up files
     world.SetupPopulationFile().SetTimingRepeat(10);
-    world.SetupResultFile("Result_"+to_string(config.SEED())+"_Ecoli_"+to_string(ECOLI_NUM)+ "_Shi_" +to_string(SHIGELLA_NUM)+".data").SetTimingRepeat(1);
+    world.SetupResultFile("Result_"+to_string(config.SEED())+"_Ecoli_"+to_string(ECOLI_NUM)+ "_Shi_" +to_string(MOI_E)+".data").SetTimingRepeat(1);
 
     //inject organisms
     for (size_t i = 0; i < ECOLI_NUM; i++){
